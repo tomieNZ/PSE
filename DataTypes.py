00388ff3507968c844cbf4f7f1af1320c5f922bc
neilzhangpro <tomiezhangnz@gmail.com>
2025-11-23 08:35:25 +1300

    DataTypes

---
+++ DataTypes.py
@@ -0,0 +1,34 @@
+"""
+Python Data Types
+This file is used to show the data types in Python.
+@author: Yaohui Zhang @tomie
+"""
+# String
+Name = "John Doe"
+
+#Integer
+Age = 28
+
+#List
+Skills = ["Python", "SQL", "Power BI"]
+
+#Tuple
+Education = ("BSc Computer Science", 2020)
+
+#Dictionary
+Contact_details = {
+    "email": "tomieweb@gmail.com",
+    "phone": "+1234567890",
+}
+
+#Set
+Certifications = {"Azure", "AWS", "Azure"}
+Certifications_removed = Certifications.remove("Azure")
+
+#print
+print(Name)
+print(Age)
+print(Skills)
+print(Education)
+print(Contact_details)
+print(Certifications_removed)
