5a5f25be5136b989cd64017d5a8b88a743d907e4
neilzhangpro <tomiezhangnz@gmail.com>
2025-11-30 10:19:10 +1300

    update commit

--- Fact_fib_class_update.py
+++ Fact_fib_class_update.py
@@ -2,8 +2,12 @@ class MathSeries:
     # No self, no staticmethod
 
     def __init__(self,n):
+        #self is the object instance, n is the argument passed to the method
+        # every time the method is called, a new object instance is created
+        # edge case: negative numbers
         if n<0:
             raise ValueError("Factorial is not defined for negative numbers.")
+        #self.n is the attribute of the object instance
         self.n = n
 
     def factorial_recursive(self):
@@ -11,6 +15,10 @@ class MathSeries:
             raise ValueError("Factorial is not defined for negative numbers.")
         if self.n in (0, 1):
             return 1
+        # Attention: the object instance is created inside the method
+        # Because if dont set the object instance, the method will not be able to access the attribute of the object instance
+        # that is not very graceful
+        # we can use @staticmethod to avoid creating a new object instance inside the method
         temp_obj = MathSeries(self.n - 1)
         return self.n * temp_obj.factorial_recursive()
 
@@ -21,6 +29,8 @@ class MathSeries:
             return 0
         if self.n == 1:
             return 1
+        # must create a new object instance inside the method
+        # Before returning the result, you must create a new object instance
         temp_obj = MathSeries(self.n - 1)
         temp_obj2 = MathSeries(self.n - 2)
         return (temp_obj.fibonacci_recursive() +
@@ -30,6 +40,7 @@ class MathSeries:
     def fibonacci_series(self):
         series = []
         for i in range(self.n + 1):
+            # must create a new object instance inside the method
             temp_obj = MathSeries(i)
             series.append(temp_obj.fibonacci_recursive())
         return series
@@ -40,8 +51,10 @@ if __name__ == "__main__":
 
     # Create an object
     obj1 = MathSeries(n)
+    # print the attribute of the object instance
     print(obj1.n)
     # Call using the object (works because no self is expected)
+    # ATTENTION: NO ARGUMENT IS PASSED TO THE METHOD
     print("Factorial (recursive):", obj1.factorial_recursive())
     print("Fibonacci (recursive):", obj1.fibonacci_recursive())
 
