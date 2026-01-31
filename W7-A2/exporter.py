2af8c088f775abcebc80af2a09765c2a2329bfeb
neilzhangpro <tomiezhangnz@gmail.com>
2026-02-01 09:49:24 +1300

    add W7-2 fixed

--- W7-A2/exporter.py
+++ W7-A2/exporter.py
@@ -5,7 +5,7 @@ import json
 class Exporter(ABC):
     """Abstract base class for exporters"""
     # Error message for when no data is provided
-    err_for_no_msg = "No data to export"
+    err_for_no_data = "No data to export"
 
     # Abstract method to export data
     @abstractmethod
@@ -16,14 +16,14 @@ class JSONExporter(Exporter):
     """Exporter for JSON format"""
     def export(self, data: dict) -> str:
         if not data:
-            return self.err_for_no_msg
+            return self.err_for_no_data
         return json.dumps(data,indent=2)
 
 class CSVExporter(Exporter):
     """Exporter for CSV format"""
     def export(self, data: dict) -> str:
         if not data:
-            return self.err_for_no_msg
+            return self.err_for_no_data
         headers = ",".join(data[0].keys())
         rows = [",".join(str(v) for v in row.values()) for row in data]
         return headers + "\n" + "\n".join(rows)
@@ -32,7 +32,7 @@ class XMLExporter(Exporter):
     """Exporter for XML format"""
     def export(self, data: dict) -> str:
         if not data:
-            return self.err_for_no_msg
+            return self.err_for_no_data
         xml_string = "<data>"
         for row in data:
             xml_string += "<row>"
